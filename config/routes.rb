73f8ce88e2e6ca0a93462f063cb2b0b0372d9611
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-07-05 10:41:55 +0100

    Sign in working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  unauthenticated do
+   root :to => 'home#index'
+end
+
+authenticated do
+  root :to => 'posts#index'
+end
+
+
   resources :posts
   root "posts#index"
 end
